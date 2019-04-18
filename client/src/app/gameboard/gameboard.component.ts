77d4e532b9c703552b0774ba168c4f7b38c377a2
David Thong Nguyen <dev@davidtn.com>
2019-04-18 12:15:58 -0700

    Styling the board and pieces

--- client/src/app/gameboard/gameboard.component.ts
+++ client/src/app/gameboard/gameboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Square, FileEnum } from './square';
+import { Piece } from './piece';
 
 @Component({
   selector: 'app-gameboard',
@@ -10,21 +11,29 @@ export class GameboardComponent implements OnInit {
   BOARD_SIZE = 8;
   board: Square[][] = [];
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor() {
     for (let i = 0; i < this.BOARD_SIZE; i++) {
       this.board.push(this.addFileSquares(i + 1));
     }
   }
 
-  // construction
+  // board construction
   addFileSquares(currRank: number): Square[] {
     const rank: Square[] = [];
     for (let i = 0; i < 8; i++) {
       rank.push(new Square(currRank, FileEnum[i + 1]));
     }
-    console.log(rank);
+    // console.log(rank);
     return rank;
+  }
+
+  // set up pieces
+  ngOnInit() {
+    for (const ranks of this.board) {
+      for (const sq of ranks) {
+        sq.piece = new Piece('pawn', 'white');
+      }
+      console.log(ranks);
+    }
   }
 }
--- client/src/app/gameboard/piece.ts
+++ client/src/app/gameboard/piece.ts
@@ -1,4 +1,12 @@
 export class Piece {
   name: string;
   color: 'white' | 'black';
+  constructor(name: string, color: 'white' | 'black') {
+    this.name = name;
+    this.color = color;
+  }
+
+  toString() {
+    return this.color + ' ' + this.name;
+  }
 }
