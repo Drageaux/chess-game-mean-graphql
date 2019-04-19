7dcb92856f783eda21bf8e6bdbb4137553bbec89
David Thong Nguyen <dev@davidtn.com>
2019-04-19 12:59:55 -0700

    Stubbed methods

--- client/src/app/gameboard/pieces/knight.ts
+++ client/src/app/gameboard/pieces/knight.ts
@@ -1,5 +1,6 @@
 import { Piece } from './piece';
 import { Square } from '../square';
+import { Move } from '../move';
 
 export class Knight extends Piece {
   constructor(color: 'white' | 'black') {
@@ -9,5 +10,8 @@ export class Knight extends Piece {
   move(board: Square[][]) {
     throw new Error('Method not implemented.');
   }
-  getAllPossibleMoves(file: string, rank: number) { }
+
+  getAllPossibleMoves(file: string, rank: number, board: Square[][]): Move[] {
+    throw new Error('Method not implemented.');
+  }
 }
--- client/src/app/gameboard/pieces/queen.ts
+++ client/src/app/gameboard/pieces/queen.ts
@@ -1,5 +1,6 @@
 import { Piece } from './piece';
 import { Square } from '../square';
+import { Move } from '../move';
 
 export class Queen extends Piece {
   constructor(color: 'white' | 'black') {
@@ -9,5 +10,8 @@ export class Queen extends Piece {
   move(board: Square[][]) {
     throw new Error('Method not implemented.');
   }
-  getAllPossibleMoves(file: string, rank: number) {}
+
+  getAllPossibleMoves(file: string, rank: number, board: Square[][]): Move[] {
+    throw new Error('Method not implemented.');
+  }
 }
