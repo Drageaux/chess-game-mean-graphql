0d625f7a97334f9429238912d3f3a5bdf9d1bc43
David Thong Nguyen <dev@davidtn.com>
2019-04-19 17:13:42 -0700

    Optimized straightline moves code and working on diagonalline moves code

--- client/src/app/gameboard/moves-getter.ts
+++ client/src/app/gameboard/moves-getter.ts
@@ -1,11 +1,16 @@
-import { Piece } from './pieces/piece';
 import { Square, FileEnum } from './square';
 import { Move } from './move';
 import { default as parser } from './gameboard-parser';
+import { Rook } from './pieces/rook';
+import { Queen } from './pieces/queen';
+import { Bishop } from './pieces/bishop';
+
+const MIN = 1;
+const MAX = 8;
 
 export default class MovesGetter {
   static getStraightLineMoves(
-    piece: Piece,
+    piece: Rook | Queen,
     file: string,
     rank: number,
     board: Square[][]
@@ -17,20 +22,19 @@ export default class MovesGetter {
     console.time('get moves in 1 loop');
 
     // find the closest piece
-    // border-inclusive if piece is enemy
-    // border-exclusive if piece is friendly
-    const MIN = 1;
-    const MAX = 8;
+    // border-inclusive if piece is enemy (capturable)
+    // border-exclusive if piece is friendly (non-capturable)
+
     // go left
     for (let i = fileEnum - 1; i >= MIN; i--) {
       const s: Square = parser.getSquare(FileEnum[i], rank, board);
       if (s) {
         if (!s.piece) {
           result.push(new Move(FileEnum[i], rank));
-        } else if (s.piece && s.piece.color === piece.color) {
-          break;
-        } else if (s.piece && s.piece.color !== piece.color) {
-          result.push(new Move(FileEnum[i], rank));
+        } else {
+          if (s.piece.color !== piece.color) {
+            result.push(new Move(FileEnum[i], rank));
+          }
           break;
         }
       }
@@ -41,10 +45,10 @@ export default class MovesGetter {
       if (s) {
         if (!s.piece) {
           result.push(new Move(FileEnum[i], rank));
-        } else if (s.piece && s.piece.color === piece.color) {
-          break;
-        } else if (s.piece && s.piece.color !== piece.color) {
-          result.push(new Move(FileEnum[i], rank));
+        } else {
+          if (s.piece.color !== piece.color) {
+            result.push(new Move(FileEnum[i], rank));
+          }
           break;
         }
       }
@@ -56,12 +60,10 @@ export default class MovesGetter {
         if (!s.piece) {
           result.push(new Move(file, i));
         } else {
-          if (s.piece.color === piece.color) {
-            break;
-          } else if (s.piece.color !== piece.color) {
+          if (s.piece.color !== piece.color) {
             result.push(new Move(file, i));
-            break;
           }
+          break;
         }
       }
     }
@@ -72,15 +74,147 @@ export default class MovesGetter {
         if (!s.piece) {
           result.push(new Move(file, i));
         } else {
-          if (s.piece.color === piece.color) {
-            break;
-          } else if (s.piece.color !== piece.color) {
+          if (s.piece.color !== piece.color) {
             result.push(new Move(file, i));
+          }
+          break;
+        }
+      }
+    }
+    // tslint:disable-next-line:no-console
+    console.timeEnd('get moves in 1 loop');
+
+    return result;
+  }
+
+  static getDiagonalLineMoves(
+    piece: Bishop | Queen,
+    file: string,
+    rank: number,
+    board: Square[][]
+  ): Move[] {
+    const result: Move[] = [];
+    const fileEnum: number = FileEnum[file];
+
+    // tslint:disable-next-line:no-console
+    console.time('get moves in 1 loop');
+
+    // find the closest piece
+    // border-inclusive if piece is enemy (capturable)
+    // border-exclusive if piece is friendly (non-capturable)
+
+    let bottomLeftStop = false;
+    let topLeftStop = false;
+    let bottomRightStop = false;
+    let topRightStop = false;
+    // go left
+    for (let distance = MIN; distance <= MAX; distance++) {
+      // go bottom-left
+
+      if (
+        !bottomLeftStop &&
+        !parser.isOutOfBound(FileEnum[fileEnum - distance], rank - distance)
+      ) {
+        const s1: Square = parser.getSquare(
+          FileEnum[fileEnum - distance],
+          rank - distance,
+          board
+        );
+        if (s1) {
+          if (!s1.piece) {
+            result.push(
+              new Move(FileEnum[fileEnum - distance], rank - distance)
+            );
+          } else {
+            if (s1.piece.color === piece.color) {
+              bottomLeftStop = true;
+            } else {
+              result.push(
+                new Move(FileEnum[fileEnum - distance], rank - distance)
+              );
+              bottomLeftStop = true;
+            }
+          }
+        }
+      }
+      // go top-left
+      if (
+        !parser.isOutOfBound(FileEnum[fileEnum - distance], rank + distance)
+      ) {
+        const s1: Square = parser.getSquare(
+          FileEnum[fileEnum - distance],
+          rank + distance,
+          board
+        );
+        if (s1) {
+          if (!s1.piece) {
+            result.push(
+              new Move(FileEnum[fileEnum - distance], rank + distance)
+            );
+          } else {
+            if (s1.piece.color !== piece.color) {
+              result.push(
+                new Move(FileEnum[fileEnum - distance], rank + distance)
+              );
+            }
+          }
+        }
+      }
+
+      // go top-left
+      /*
+      for (let j = rank + 1; j <= MAX; j++) {
+        const s: Square = parser.getSquare(FileEnum[i], j, board);
+        if (s) {
+          if (!s.piece) {
+            result.push(new Move(FileEnum[i], rank));
+          } else if (s.piece && s.piece.color === piece.color) {
+            break;
+          } else if (s.piece && s.piece.color !== piece.color) {
+            result.push(new Move(FileEnum[i], rank));
             break;
           }
         }
       }
+      */
     }
+    /*
+    // go right
+    for (let i = fileEnum + 1; i <= MAX; i++) {
+      // go top-right
+      for (let j = rank + 1; j <= MAX; j++) {
+        const s: Square = parser.getSquare(FileEnum[i], j, board);
+        if (s) {
+          if (!s.piece) {
+            result.push(new Move(FileEnum[i], j));
+          } else {
+            if (s.piece.color === piece.color) {
+              break;
+            } else if (s.piece.color !== piece.color) {
+              result.push(new Move(FileEnum[i], j));
+              break;
+            }
+          }
+        }
+      }
+      // go bottom-right
+      for (let j = rank - 1; j >= MIN; j--) {
+        const s: Square = parser.getSquare(FileEnum[i], j, board);
+        if (s) {
+          if (!s.piece) {
+            result.push(new Move(FileEnum[i], j));
+          } else {
+            if (s.piece.color === piece.color) {
+              break;
+            } else if (s.piece.color !== piece.color) {
+              result.push(new Move(FileEnum[i], j));
+              break;
+            }
+          }
+        }
+      }
+      */
+
     // tslint:disable-next-line:no-console
     console.timeEnd('get moves in 1 loop');
 
