19bc49089f8de41a588edae9d535b4458cb53dc6
David Thong Nguyen <dev@davidtn.com>
2019-04-30 14:52:51 -0700

    Made a getter for Piece id

--- client/src/app/gameboard/pieces/piece.ts
+++ client/src/app/gameboard/pieces/piece.ts
@@ -5,20 +5,25 @@ import { map } from 'rxjs/operators';
 import { v4 as uuid } from 'uuid';
 
 export abstract class Piece {
-  id: string;
+  // tslint:disable-next-line:variable-name
+  private _id: string;
   name: string;
   color: 'white' | 'black';
   myFile: string;
   myRank: number;
 
   constructor(name: string, color: 'white' | 'black') {
-    this.id = uuid();
+    this._id = uuid();
     this.name = name;
     this.color = color;
   }
 
   toString() {
     return `${this.color} ${this.name}`;
+  }
+
+  get id() {
+    return this._id;
   }
 
   getAttackMoves(
