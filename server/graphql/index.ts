7e24b31c74de8d770e26855a26b77fc0bfccadd7
David Thong Nguyen <dev@davidtn.com>
2019-05-03 14:42:19 -0700

    Rewriting GraphQL using Apollo server module with Express support

---
+++ server/graphql/index.ts
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server-express');
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+];
+// The GraphQL schema
+export const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+  }
+`;
+// A map of functions which return data for the schema.
+export const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,11 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
-const mongoose = require('mongoose');
-const graphqlHTTP = require('express-graphql');
 const compression = require('compression');
 const cors = require('cors');
-import { execute, subscribe } from 'graphql';
+const { ApolloServer, gql } = require('apollo-server-express');
 import { PubSub } from 'graphql-subscriptions';
+import { execute, subscribe } from 'graphql';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 
 const env = process.env.NODE_ENV || 'development';
@@ -18,20 +17,15 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use('*', cors());
 app.use(compression());
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
 
 // setup MongoDB
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/chess-game-mean-graphql', {
   useNewUrlParser: true
 });
 var db = mongoose.connection
-  .on('error', err => {
+  .on('error', (err: any) => {
     console.log(
       'Error: Could not connect to MongoDB. Did you forget to run `mongod`?'
     );
@@ -41,20 +35,29 @@ var db = mongoose.connection
   });
 
 // setup GraphQL
-const userSchema = require('./graphql/index').userSchema;
-app.use(
-  '/graphql',
-  cors(),
-  graphqlHTTP({
-    schema: userSchema,
-    rootValue: global,
-    graphiql: true
-  })
-);
+// const userSchema = require('./graphql/index').userSchema;
+import { typeDefs, resolvers } from './graphql';
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+server.applyMiddleware({ app }); // app is from an existing express app
 
-// Up and Running at Port 3000
+// point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+// catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// up and running at port 3000
 app.listen(process.env.PORT || 3000, () => {
-  console.log('A GraphQL API running at port 3000');
+  console.log(
+    `🚀 Server ready at http://localhost:${process.env.PORT || 3000}${
+      server.graphqlPath
+    }`
+  );
+  /*
   new SubscriptionServer(
     {
       execute,
@@ -66,4 +69,5 @@ app.listen(process.env.PORT || 3000, () => {
       path: '/subscriptions'
     }
   );
+  */
 });
