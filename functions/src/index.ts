3d9735229b3b00c17829412be25ce6d4ceee7dfb
David Thong Nguyen <dev@davidtn.com>
2019-06-18 11:47:11 -0700

    Basic functions for testing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,40 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original: string = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin
+    .database()
+    .ref('/messages')
+    .push({ original });
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
+
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+export const makeUppercase = functions.database
+  .ref('/messages/{pushId}/original')
+  .onCreate((snapshot, context) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const original: string = snapshot.val();
+    console.log('Uppercasing', context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return snapshot.ref.parent!.child('uppercase').set(uppercase);
+  });
